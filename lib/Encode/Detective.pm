4126374e5c3c3110fa115b4a309022935976a7de
Ben Bullock <bkb@cpan.org>
2013-11-30 04:01:32 +0000

    Import of BKB/Encode-Detective-0.05 from CPAN.
    gitpan-cpan-distribution: Encode-Detective
    gitpan-cpan-version:      0.05
    gitpan-cpan-path:         BKB/Encode-Detective-0.05.tar.gz
    gitpan-cpan-author:       BKB
    gitpan-cpan-maturity:     released

--- lib/Encode/Detective.pm
+++ lib/Encode/Detective.pm
@@ -1,39 +1,3 @@
-# * ***** BEGIN LICENSE BLOCK *****
-# Version: MPL 1.1/GPL 2.0/LGPL 2.1
-#
-# The contents of this file are subject to the Mozilla Public License Version
-# 1.1 (the "License"); you may not use this file except in compliance with
-# the License. You may obtain a copy of the License at
-# http://www.mozilla.org/MPL/
-#
-# Software distributed under the License is distributed on an "AS IS" basis,
-# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
-# for the specific language governing rights and limitations under the
-# License.
-#
-# The Original Code is Encode::Detect wrapper
-#
-# The Initial Developer of the Original Code is
-# Proofpoint, Inc.
-# Portions created by the Initial Developer are Copyright (C) 2005
-# the Initial Developer. All Rights Reserved.
-#
-# Contributor(s):
-#
-# Alternatively, the contents of this file may be used under the terms of
-# either the GNU General Public License Version 2 or later (the "GPL"), or
-# the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
-# in which case the provisions of the GPL or the LGPL are applicable instead
-# of those above. If you wish to allow use of your version of this file only
-# under the terms of either the GPL or the LGPL, and not to allow others to
-# use your version of this file under the terms of the MPL, indicate your
-# decision by deleting the provisions above and replace them with the notice
-# and other provisions required by the GPL or the LGPL. If you do not delete
-# the provisions above, a recipient may use your version of this file under
-# the terms of any one of the MPL, the GPL or the LGPL.
-#
-# ***** END LICENSE BLOCK *****
-
 package Encode::Detective;
 require Exporter;
 @ISA = qw(Exporter);
@@ -43,21 +7,22 @@ use strict;
 use warnings;
 
 use XSLoader;
-our $VERSION = 0.04;
+our $VERSION = 0.05;
 XSLoader::load 'Encode::Detective', $VERSION;
 
 1;
 
 __END__
 
+=encoding UTF-8
+
 =head1 NAME
 
-Encode::Detective - detect a data encoding
+Encode::Detective - guess the encoding of text
 
 =head1 SYNOPSIS
 
-  use Encode;
-  require Encode::Detective 'detect';
+  use Encode::Detective 'detect';
   my $encoding = detect ($data);
   # Now $encoding contains a guess of the encoding of $data.
 
@@ -70,12 +35,22 @@ L<Encode::Guess>, but does not require a list of expected encodings.
 
 =head2 detect
 
-    my $encoding = detect ($data);
+    my $encoding = detect ($text);
 
-Given a set of bytes, C<$data>, this looks at the bytes, and guesses
-what encoding they are encoded in using probabilities.
+Given some bytes of text, C<$text>, this guesses what encoding they
+are in. If the encoding cannot be guessed, or if it seems to be ASCII,
+the undefined value is returned. C<$encoding> can then be passed to
+the C<decode> method of L<Encode>:
 
-=head1 DETECTED ENCODINGS
+    use Encode 'decode';
+    my $encoding = detect ($text);
+    if ($encoding) {
+        $text = decode ($encoding, $text);
+    }
+
+=head1 DETECTED AND UNDETECTED ENCODINGS
+
+=head2 Detected encodings
 
 The following encodings are detected:
 
@@ -83,13 +58,23 @@ The following encodings are detected:
 
 =item UTF-8
 
+Unicode.
+
 =item EUC-JP
+
+Japanese Extended Unix Code.
 
 =item Big5
 
+Chinese encoding.
+
 =item Shift_JIS
 
+Japanese Microsoft encoding.
+
 =item EUC-KR
+
+Korean Extended Unix Code.
 
 =item EUC-TW
 
@@ -103,61 +88,117 @@ Cyrillic encoding.
 
 Hebrew encoding.
 
+=item windows-1252
+
+French/European encoding.
+
 =back
 
-Character sets not detected
+=head2 Undetected encodings
+
+The following character sets are not detected:
 
 =over
 
 =item mac roman
 
+A MacIntosh encoding incorporating some European letters.
+
 =item CP932
 
 An extension of Shift-JIS, more common in practice than actual
-Shift-JIS.
+Shift-JIS. This has code points for things like ① (circled one) which
+don't exist in Shift-JIS.
+
+=item TIS-620
+
+A Thai encoding.
 
 =back
 
-=head1 TODO
-
-The module needs more tests. Please send example files
-
 =head1 BUGS
 
-=head2 TIS-620
+=over
+
+=item Lacks regression tests
+
+The module needs many, many more regression tests before any work can
+be done on altering the underlying algorithms. The existing library
+already has checks for some of the encodings which are not
+detected. These are currently switched off for some reason, maybe
+because it was not possible to unambiguously detect them. Without very
+solid regression tests, no patches can be applied to the detection
+code, since it will not be clear whether or not the existing encoding
+detection for every possible encoding is damaged by the
+patches. 
+
+Please send example files containing encodings. Either fork the
+repository and add files in the directory F<t/samples>, or send them
+to the module maintainer at <bkb@cpan.org>.
+
+=item TIS-620
 
 TIS-620 does not seem to be detected.
 
-=head2 Documentation of detection
+=item Documentation of detection
 
 The documentation of detected encodings above is not complete.
 
+=back
+
 =head1 HISTORY
 
-This module is based on code of Firefox. When this module was created,
-the C++ code for character set detection was available as a standalone
-library. Now the code cannot be used as a standalone library, so this
-has become a fork of the original Mozilla code.
+Encode::Detective is based on the C++ library for character set
+detection in the Firefox web browser. This library used to be
+available as a standalone library. Unfortunately, as of 2012, the
+Firefox code has been integrated into the browser code, and it cannot
+be used as a standalone library, so this has become a fork of the
+original Mozilla code.
 
 Encode::Detective is a fork of L<Encode::Detect>. It removes almost
 all of the interface of Encode::Detect except the single function
-L</detect>. This fork was released to CPAN to improve the compilation
-of the module on various systems.
+L</detect>. This fork is intended to improve the compilation of the
+module on various systems. It was released to CPAN to access CPAN
+testers.
 
 =head1 SEE ALSO
 
 =head2 edetect
 
-The L<edetect> standalone script can guess the encodings of files.
+The L<edetect> standalone script installed with Encode::Detective can
+guess the encodings of files.
+
+=head2 Online demonstration
+
+L<LeMoDa.net offers an online detection
+service|http://www.lemoda.net/tools/encode-detective/>, which also
+checks the HTTP response header and the meta tag (the tag containing
+C<charset=>) of the page.
 
 =head2 Encode::Guess
 
 L<Encode::Guess> is a Perl module which does something similar to
-Encode::Detective.
+Encode::Detective. It is slightly different in that it requires a list
+of candidate encodings.
 
 =head2 Encode::Detect
 
-The original version of this module.
+L<Encode::Detect> is the original version of this module. Whereas this
+module offers the single interface, L</detect>, Encode::Detect has
+facilities to create an object, make multiple reads, and detect end of
+file.
+
+=head2 IO::HTML
+
+L<IO::HTML> uses byte order mark inspection and inspection of HTML to
+detect encodings of web pages (see
+L<http://en.wikipedia.org/wiki/Byte_order_mark>).
+
+=head2 C++ and Perl XS
+
+There is a short description of the method used in this module to
+combine C++ and Perl XS at
+L<http://www.lemoda.net/perl/xs-and-cplusplus/>.
 
 =head1 AUTHORS
 
